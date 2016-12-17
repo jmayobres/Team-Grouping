4759379d609656a9b757bb76532489d3ed2ed3bb
jmayobres <jmayobres@gmail.com>
2016-12-17 16:34:35 -0500

    Initial Commit

---
+++ Team GroupingTests/Team_GroupingTests.swift
@@ -0,0 +1,36 @@
+//
+//  Team_GroupingTests.swift
+//  Team GroupingTests
+//
+//  Created by Juan Manuel Mayobre on 12/17/16.
+//  Copyright © 2016 Juan Manuel Mayobre. All rights reserved.
+//
+
+import XCTest
+@testable import Team_Grouping
+
+class Team_GroupingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
